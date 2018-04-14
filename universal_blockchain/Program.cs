90005097989b6881b76dd059d527b7291fe591bd
Emelin Nikita <clever899@gmail.com>
2018-04-14 20:37:52 +0300

    Добавьте файлы проекта.

---
+++ universal_blockchain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace universal_blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
