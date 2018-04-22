67d08645605e6cc70c718841b1093c73006e6ea6
Roman <thirtybeltmusicgroup@gmail.com>
2018-04-22 22:27:06 +0300

    Fix4last commit
    Signed-off-by: Roman <thirtybeltmusicgroup@gmail.com>

--- universal_blockchain/Configuration.cs
+++ universal_blockchain/Configuration.cs
@@ -11,14 +11,10 @@ namespace universal_blockchain
 
 		public static ILog GetLogger()
 		{
-<<<<<<< HEAD
 			if (logger != null)
 				return logger;
-
-			GlobalContext.Properties["LogFileName"] = "AppName";
-=======
+			
 			GlobalContext.Properties["LogFileName"] = "Universal_Blockchain";
->>>>>>> 0b154ae21a4607539b560950a39fb8f98fa58ddd
 			var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
 			XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
